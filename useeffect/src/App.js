396296f7abd925d3e8d94fc2d2d939319d3b7d73
MyeongHoonNam <codenmh0822@naver.com>
2021-10-17 16:07:03 +0900

    useEffect 다루기

--- useeffect/src/App.js
+++ useeffect/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+// useEffect는 특정 요소의 변화가 있을 때 반응성으로 감지하여 나타나는 Hook이다.
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log(`Clicked ${count} times.`);
+  }, [count]); // count 변화 감지
+
+  useEffect(() => {
+    // 라이프사이클 처럼의 활용
+    console.log('Component Loaded');
+
+    const handleScroll = () => {
+      console.log(window.scrollY);
+    };
+
+    document.addEventListener('scroll', handleScroll); // 전역 이벤트 사용할 때 활용
+
+    return () => document.removeEventListener('scroll', handleScroll); // return으로 반환하는 함수는 컴포넌트 제거시 사용된다.
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>You Clicked {count} times.</div>
+      <button onClick={() => setCount(count + 1)}>+</button>
+
+      <div style={{ height: 10000 }}></div>
     </div>
   );
 }
