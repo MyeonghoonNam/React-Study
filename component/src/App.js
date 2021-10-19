ec503955723d7ce9c0390614ca99870c5ccdd86e
MyeongHoonNam <codenmh0822@naver.com>
2021-10-19 23:48:39 +0900

    css 함수를 활용한 emotion 다루기

--- component/src/App.js
+++ component/src/App.js
@@ -1,12 +1,25 @@
 import './App.css';
 import Logo from './components/Logo';
 import Paragraph from './components/Paragraph';
+import { css } from '@emotion/react';
+
+const style = css`
+  color: hotpink;
+`;
+
+const SomeComponent = ({ children }) => (
+  <div css={style}>
+    Some hotpink text.
+    {children}
+  </div>
+);
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <Logo size={300}/>
+    <div className="App">
+      <SomeComponent />
+      <header className="App-header">
+        <Logo size={300} />
         <Logo />
         {/* <Logo size='100px'/> */}
         <Paragraph>
@@ -17,10 +30,10 @@ function App() {
           Edit blue code
         </Paragraph>
         <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
         >
           Learn React
         </a>
