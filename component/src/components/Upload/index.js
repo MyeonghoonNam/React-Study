2f57cb436d7dc72c0c4d09ed1d3058e5c8e87893
MyeongHoonNam <codenmh0822@naver.com>
2021-10-21 17:48:49 +0900

    Upload 컴포넌트 구현

---
+++ component/src/components/Upload/index.js
@@ -0,0 +1,101 @@
+import styled from '@emotion/styled';
+import { useRef, useState } from 'react';
+
+const UploadContainer = styled.div`
+  display: inline-block;
+  cursor: pointer;
+`;
+
+const Input = styled.input`
+  display: none;
+`;
+
+const Upload = ({
+  children,
+  droppable,
+  name,
+  accept,
+  value,
+  onChange,
+  ...props
+}) => {
+  const [file, setFile] = useState(value);
+  const [dragging, setDragging] = useState(false);
+
+  const inputRef = useRef(null);
+
+  const handleFileChange = (e) => {
+    const files = e.target.files;
+    const changedFile = files[0];
+
+    setFile(changedFile);
+    onChange && onChange(changedFile);
+  };
+
+  const handleChooseFile = () => {
+    inputRef.current.click();
+  };
+
+  const handleDragEnter = (e) => {
+    if (!droppable) return;
+
+    e.preventDefault(); // 브라우저 기본 이벤트 억제
+    e.stopPropagation(); // 부모나 자식 컴포넌트 이벤트 전파 방지
+
+    if (e.dataTransfer.items && e.dataTransfer.items.length > 0) {
+      setDragging(true);
+    }
+  };
+
+  const handleDragLeave = (e) => {
+    if (!droppable) return;
+
+    e.preventDefault();
+    e.stopPropagation();
+
+    setDragging(false);
+  };
+
+  const handleDragOver = (e) => {
+    if (!droppable) return;
+
+    e.preventDefault();
+    e.stopPropagation();
+  };
+
+  const handleFileDrop = (e) => {
+    if (!droppable) return;
+
+    e.preventDefault();
+    e.stopPropagation();
+
+    const files = e.dataTransfer.files;
+    const changedFile = files[0];
+
+    setFile(changedFile);
+    onChange && onChange(changedFile);
+    setDragging(false);
+  };
+
+  return (
+    <UploadContainer
+      onClick={handleChooseFile}
+      onDrop={handleFileDrop}
+      onDragEnter={handleDragEnter}
+      onDragLeave={handleDragLeave}
+      onDragOver={handleDragOver}
+      {...props}
+    >
+      <Input
+        ref={inputRef}
+        type="file"
+        name={name}
+        accept={accept}
+        onChange={handleFileChange}
+      />
+      {typeof children === 'function' ? children(file, dragging) : children}
+    </UploadContainer>
+  );
+};
+
+export default Upload;
---
+++ component/src/stories/Upload.stories.js
@@ -0,0 +1,41 @@
+import Upload from '../components/Upload';
+
+export default {
+  title: 'Component/Upload',
+  component: Upload,
+};
+
+export const Default = () => {
+  return (
+    <Upload>
+      <button>Click me !</button>
+    </Upload>
+  );
+};
+
+export const AccessFile = () => {
+  return (
+    <Upload>
+      {(file) => <button>{file ? file.name : 'Click me !'}</button>}
+    </Upload>
+  );
+};
+
+export const Droppable = () => {
+  return (
+    <Upload droppable>
+      {(file, dragging) => (
+        <div
+          style={{
+            width: 300,
+            height: 100,
+            border: '4px dashed #aaa',
+            borderColor: dragging ? 'black' : '#aaa',
+          }}
+        >
+          {file ? file.name : 'Click or drag file to this area to upload.'}
+        </div>
+      )}
+    </Upload>
+  );
+};
