e9412066cb7db5196619e500771df36264128ce7
MyeongHoonNam <codenmh0822@naver.com>
2021-10-21 19:55:34 +0900

    Badge 컴포넌트를 활용한 알림 UI 구현

---
+++ component/src/stories/Badge.stories.js
@@ -0,0 +1,51 @@
+import Badge from '../components/Badge';
+import Image from '../components/Image';
+
+export default {
+  title: 'Component/Badge',
+  component: Badge,
+  argTypes: {
+    count: {
+      defaultValue: 10,
+      control: 'number',
+    },
+    maxCount: {
+      defaultValue: 100,
+      control: 'number',
+    },
+    backgroundColor: {
+      control: 'color',
+    },
+    textColor: {
+      control: 'color',
+    },
+    showCount: {
+      defaultValue: false,
+      control: 'boolean',
+    },
+  },
+};
+
+export const Default = (args) => {
+  return (
+    <Badge {...args}>
+      <Image
+        src="https://picsum.photos/60"
+        width={60}
+        style={{ borderRadius: 8 }}
+      />
+    </Badge>
+  );
+};
+
+export const Dot = () => {
+  return (
+    <Badge dot>
+      <Image
+        src="https://picsum.photos/60"
+        width={40}
+        style={{ borderRadius: 8 }}
+      />
+    </Badge>
+  );
+};
