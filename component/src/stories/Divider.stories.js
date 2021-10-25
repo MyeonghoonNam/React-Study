1641f9e9b8937ec470bb88a1d2066cb61ddcc58e
MyeongHoonNam <codenmh0822@naver.com>
2021-10-25 17:38:20 +0900

    Divider 컴포넌트 구현 연습

---
+++ component/src/stories/Divider.stories.js
@@ -0,0 +1,27 @@
+import Divider from '../components/Divider';
+import Text from '../components/Text';
+
+export default {
+  title: 'Component/Divider',
+  component: Divider,
+};
+
+export const Horizontal = () => {
+  return (
+    <>
+      <Text>위</Text>
+      <Divider type="horizontal" />
+      <Text>아래</Text>
+    </>
+  );
+};
+
+export const Vertical = () => {
+  return (
+    <>
+      <Text>왼쪽</Text>
+      <Divider type="vertical" />
+      <Text>오른쪽</Text>
+    </>
+  );
+};
