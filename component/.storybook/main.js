ae00745c3c2ad64628b87be60395952a9d5a00d7
MyeongHoonNam <codenmh0822@naver.com>
2021-10-28 00:40:12 +0900

    storybook, react alias 설정

--- component/.storybook/main.js
+++ component/.storybook/main.js
@@ -1,11 +1,24 @@
+const customWebpackConfig = require('../craco.config.js');
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
-  ]
-}
+  webpackFinal: async (config) => {
+    const { webpack } = customWebpackConfig;
+
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+          ...webpack.alias,
+        },
+      },
+    };
+  },
+};
--- component/craco.config.js
+++ component/craco.config.js
@@ -1,5 +1,18 @@
+const path = require('path');
+
 module.exports = {
   babel: {
     presets: ['@emotion/babel-preset-css-prop'],
+  },
+  webpack: {
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@contexts': path.resolve(__dirname, 'src/contexts'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@constants': path.resolve(__dirname, 'src/constants'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+    },
   },
 };
