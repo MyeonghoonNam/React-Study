18a8246b23ffa6956df63a76497ec350142d9546
MyeongHoonNam <codenmh0822@naver.com>
2021-11-09 19:16:16 +0900

    클래스형 컴포넌트에서 props 사용하기

--- component-theory/src/MyComponent.js
+++ component-theory/src/MyComponent.js
@@ -1,26 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const MyComponent = ({ name, age }) => {
-  return (
-    <div>
-      이름 {name}
-      <br />
-      나이 {age}
-    </div>
-  );
-};
+// 클래스형 컴포넌트
+class MyComponent extends Component {
+  static defaultProps = {
+    name: 'Hoon',
+    age: '26',
+  };
 
-MyComponent.defaultProps = {
-  name: 'Hoon',
-};
+  static propTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number.isRequired,
+  };
 
-MyComponent.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number.isRequired,
+  render() {
+    const { name, age } = this.props;
 
-  // PropTypes에는 다양한 종류가 존재한다.
-  // 더 자세한 정보는  https://github.com/facebook/prop-types 참고하자
-};
+    return (
+      <div>
+        이름 {name}
+        <br />
+        나이 {age}
+      </div>
+    );
+  }
+}
+
+// 함수형 컴포넌트
+// const MyComponent = ({ name, age }) => {
+//   return (
+//     <div>
+//       이름 {name}
+//       <br />
+//       나이 {age}
+//     </div>
+//   );
+// };
+
+// MyComponent.defaultProps = {
+//   name: 'Hoon',
+// };
+
+// MyComponent.propTypes = {
+//   name: PropTypes.string,
+//   age: PropTypes.number.isRequired,
+
+//   // PropTypes에는 다양한 종류가 존재한다.
+//   // 더 자세한 정보는  https://github.com/facebook/prop-types 참고하자
+// };
 
 export default MyComponent;
