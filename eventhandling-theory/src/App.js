41b3643879781d3522be091e15eb647d94c453ce
MyeongHoonNam <codenmh0822@naver.com>
2021-11-10 18:25:39 +0900

    form 값을 state로 관리

---
+++ eventhandling-theory/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import EventPractice from './EventPractice';
+
+function App() {
+  return <EventPractice />;
+}
+
+export default App;
---
+++ eventhandling-theory/src/EventPractice.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+class EventPractice extends Component {
+  state = {
+    message: '',
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      message: e.target.value,
+    });
+  };
+
+  handleClick = () => {
+    alert(this.state.message);
+
+    this.setState({
+      message: '',
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>이벤트 연습</h1>
+        <input
+          type="text"
+          name="message"
+          placeholder="input..."
+          value={this.state.message}
+          onChange={this.handleChange}
+        />
+        <button onClick={this.handleClick}>확인</button>
+      </div>
+    );
+  }
+}
+
+export default EventPractice;
