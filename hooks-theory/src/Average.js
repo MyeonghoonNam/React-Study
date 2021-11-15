e2ebb540e5c55f55f0dfd67388328801ad31e2a7
MyeongHoonNam <codenmh0822@naver.com>
2021-11-15 19:51:42 +0900

    useRef 사용법

--- hooks-theory/src/Average.js
+++ hooks-theory/src/Average.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, useCallback } from 'react';
+import React, { useState, useMemo, useCallback, useRef } from 'react';
 
 const getAverage = (numbers) => {
   console.log('평균값 계산 중...');
@@ -16,6 +16,7 @@ const getAverage = (numbers) => {
 const Average = () => {
   const [number, setNumber] = useState(0);
   const [list, setList] = useState([]);
+  const inputEl = useRef(null); // useRef Hook은 함수형 컴포넌트에서 ref를 쉽게 사용할 수 있도록 해 줍니다.
 
   // useCallback은 useMemo와 상당히 비슷한 함수입니다. 주로 렌더링 성능을 최적화해야 하는 상황에서 사용하는데요. 이 Hook을 사용하면 이벤트 핸들러 함수를 필요할 때만 생성할 수 있습니다.
   //  onChange와 onInsert라는 함수를 이렇게 선언하면 컴포넌트가 리렌더링될 때마다 이 함수들이 새로 생성됩니다. 대부분의 경우 이러한 방식은 문제없지만, 컴포넌트의 렌더링이 자주 발생하거나 렌더링해야 할 컴포넌트의 개수가 많아지면 이 부분을 최적화해 주는 것이 좋습니다.
@@ -27,8 +28,11 @@ const Average = () => {
 
   const onClick = useCallback(() => {
     const nextList = list.concat(parseInt(number));
+
     setList(nextList);
     setNumber('');
+
+    inputEl.current.focus();
   }, [number, list]); // 렌더링, number 혹은 list가 바뀌었을 때만 함수를 다시 생성
 
   // useMemo를 사용하면 함수형 컴포넌트 내부에서 발생하는 연산을 최적화할 수 있습니다.
@@ -37,7 +41,7 @@ const Average = () => {
 
   return (
     <div>
-      <input value={number} onChange={onChange} />
+      <input ref={inputEl} value={number} onChange={onChange} />
       <button onClick={onClick}>등록</button>
       <ul>
         {list.map((value, index) => (
