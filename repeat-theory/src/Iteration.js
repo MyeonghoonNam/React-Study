70ea05ed5db1661b41bb22c5a088a4232132478a
MyeongHoonNam <codenmh0822@naver.com>
2021-11-15 16:35:16 +0900

    데이터 제거 기능 구현

--- repeat-theory/src/Iteration.js
+++ repeat-theory/src/Iteration.js
@@ -15,6 +15,7 @@ const Iteration = () => {
   };
 
   const onClick = () => {
+    // 리액트에서 상태를 업데이트할 때는 기존 상태를 그대로 두면서 새로운 값을 상태로 설정해야 한다. 이를 불변성 유지라고 하며 불변성 유지를 해 주어야 나중에 리액트 컴포넌트의 성능을 최적화할 수 있다.
     const nextNames = names.concat({
       id: nextId,
       text: inputText,
@@ -25,12 +26,23 @@ const Iteration = () => {
     setInputText('');
   };
 
+  const onRemove = (id) => {
+    const nextNames = names.filter((name) => name.id !== id);
+
+    setNames(nextNames);
+  };
+
   // 리액트에서 key는 컴포넌트 배열을 렌더링했을 때 어떤 원소에 변동이 있었는지 알아내려고 사용한다.
   // 예를 들어 유동적인 데이터를 다룰 때는 원소를 새로 생성할 수도, 제거할 수도, 수정할 수도 있죠. key가 없을 때는 Virtual DOM을 비교하는 과정에서 리스트를 순차적으로 비교하면서 변화를 감지한다.
   //하지만 key가 있다면 이 값을 사용하여 어떤 변화가 일어났는지 더욱 빠르게 알아낼 수 있다.
   // key 값은 언제나 유일해야 한다. 따라서 데이터가 가진 고윳값을 key 값으로 설정해야 한다.
   // index를 key로 사용하면 배열이 변경될 때 효율적으로 리렌더링하지 못하는 단점이 있다.
-  const nameList = names.map((name) => <li key={name.id}>{name.text}</li>);
+  // 상태 안에서 배열을 변형할 때는 배열에 직접 접근하여 수정하는 것이 아니라 concat, filter 등의 배열 내장 함수를 사용하여 새로운 배열을 만든 후 이를 새로운 상태로 설정해 주어야 한다.
+  const nameList = names.map((name) => (
+    <li key={name.id} onDoubleClick={() => onRemove(name.id)}>
+      {name.text}
+    </li>
+  ));
 
   return (
     <>
