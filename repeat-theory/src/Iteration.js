41eabfd607c04669da158175d4efc5bfabb52647
MyeongHoonNam <codenmh0822@naver.com>
2021-11-15 16:28:49 +0900

    데이터 생성 기능 구현

--- repeat-theory/src/Iteration.js
+++ repeat-theory/src/Iteration.js
@@ -1,16 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Iteration = () => {
-  const names = ['눈사람', '얼음', '눈', '바람'];
+  const [names, setNames] = useState([
+    { id: 1, text: '눈사람' },
+    { id: 2, text: '얼음' },
+    { id: 3, text: '눈' },
+    { id: 4, text: '바람' },
+  ]);
+  const [inputText, setInputText] = useState('');
+  const [nextId, setNextId] = useState(5);
+
+  const onChange = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const onClick = () => {
+    const nextNames = names.concat({
+      id: nextId,
+      text: inputText,
+    });
+
+    setNextId(nextId + 1);
+    setNames(nextNames);
+    setInputText('');
+  };
 
   // 리액트에서 key는 컴포넌트 배열을 렌더링했을 때 어떤 원소에 변동이 있었는지 알아내려고 사용한다.
   // 예를 들어 유동적인 데이터를 다룰 때는 원소를 새로 생성할 수도, 제거할 수도, 수정할 수도 있죠. key가 없을 때는 Virtual DOM을 비교하는 과정에서 리스트를 순차적으로 비교하면서 변화를 감지한다.
   //하지만 key가 있다면 이 값을 사용하여 어떤 변화가 일어났는지 더욱 빠르게 알아낼 수 있다.
   // key 값은 언제나 유일해야 한다. 따라서 데이터가 가진 고윳값을 key 값으로 설정해야 한다.
   // index를 key로 사용하면 배열이 변경될 때 효율적으로 리렌더링하지 못하는 단점이 있다.
-  const nameList = names.map((name, index) => <li key={index}>{name}</li>);
+  const nameList = names.map((name) => <li key={name.id}>{name.text}</li>);
 
-  return <ul>{nameList}</ul>;
+  return (
+    <>
+      <input value={inputText} onChange={onChange} />
+      <button onClick={onClick}>추가</button>
+      <ul>{nameList}</ul>;
+    </>
+  );
 };
 
 export default Iteration;
