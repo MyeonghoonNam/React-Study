1b0fb7a00116ffdf8efb9d7f8b7a58a49f995e59
MyeongHoonNam <codenmh0822@naver.com>
2021-11-16 17:09:22 +0900

    styled-component 사용법

--- styling-react-theory/src/App.js
+++ styling-react-theory/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import SassComponent from './SassComponent';
+// import SassComponent from './SassComponent';
+import StyledComponent from './StyledComponent';
 class App extends Component {
   render() {
     return (
       <div>
-        <SassComponent />
+        <StyledComponent />
       </div>
     );
   }
