0b304108a4fbb879c26d378d9b44474acac8e889
MyeongHoonNam <codenmh0822@naver.com>
2021-11-18 22:58:46 +0900

    React.memo를 통한 최적화

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import TodoListItem from './TodoListItem';
 import styled from '@emotion/styled';
 
@@ -22,4 +23,4 @@ const TodoList = ({ todos, onRemove, onToggle }) => {
   );
 };
 
-export default TodoList;
+export default React.memo(TodoList);
