651594ebe9de20d3f22c433b1e64dfa9bd2f60a3
MyeongHoonNam <codenmh0822@naver.com>
2021-11-18 15:47:50 +0900

    TodoInsert 컴포넌트 UI 구현

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,7 +1,12 @@
+import TodoInsert from './components/TodoInsert';
 import TodoTemplate from './components/TodoTemplate';
 
 const App = () => {
-  return <TodoTemplate>Todo App</TodoTemplate>;
+  return (
+    <TodoTemplate>
+      <TodoInsert />
+    </TodoTemplate>
+  );
 };
 
 export default App;
