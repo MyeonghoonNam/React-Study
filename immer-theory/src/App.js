07419de3b6281fbf81c52d230e0ee5caf2a7aadc
MyeongHoonNam <codenmh0822@naver.com>
2021-11-25 16:31:32 +0900

    immer를 활용한 불변성 유지를 통한 상태 업데이트

--- immer-theory/src/App.js
+++ immer-theory/src/App.js
@@ -1,23 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState, useCallback } from 'react';
+import produce from 'immer';
 
 function App() {
+  const nextId = useRef(1);
+  const [form, setForm] = useState({ name: '', username: '' });
+  const [data, setData] = useState({
+    array: [],
+    uselessValue: null,
+  });
+
+  const onChange = useCallback(
+    (e) => {
+      const { name, value } = e.target;
+
+      setForm(
+        // immer를 사용하면 depth가 깊은 상태의 경우에 불변성을 유지하는 작업을 매우 간단하게 처리할 수 있다.
+        // 첫 번째 파라미터는 수정하고 싶은 상태이고, 두 번째 파라미터는 상태를 어떻게 업데이트할지 정의하는 함수
+        // 두 번째 파라미터로 전달되는 함수 내부에서 원하는 값을 변경하면, produce 함수가 불변성 유지를 대신해 주면서 새로운 상태를 생성해 줍니다.
+        //  불변성을 유지하는 코드가 복잡할 때만 사용하자
+        produce(form, (draft) => {
+          draft[name] = value;
+        }),
+      );
+    },
+    [form],
+  );
+
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+
+      const info = {
+        id: nextId.current,
+        name: form.name,
+        username: form.username,
+      };
+
+      setData(
+        produce(data, (draft) => {
+          draft.array.push(info);
+        }),
+      );
+
+      setForm({
+        name: '',
+        username: '',
+      });
+
+      nextId.current += 1;
+    },
+    [data, form.name, form.username],
+  );
+
+  const onRemove = useCallback(
+    (id) => {
+      setData(
+        produce(data, (draft) => {
+          draft.array.splice(
+            draft.array.findIndex((info) => info.id === id),
+            1,
+          );
+        }),
+      );
+    },
+    [data],
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="username"
+          placeholder="id"
+          value={form.username}
+          onChange={onChange}
+        />
+        <input
+          name="name"
+          placeholder="name"
+          value={form.name}
+          onChange={onChange}
+        />
+        <button type="submit">등록</button>
+      </form>
+      <div>
+        <ul>
+          {data.array.map((info) => (
+            <li key={info.id} onClick={() => onRemove(info.id)}>
+              {info.username} ({info.name})
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
