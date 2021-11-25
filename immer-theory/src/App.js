14717dea2e9bf98df7df042c5212518ca414a305
MyeongHoonNam <codenmh0822@naver.com>
2021-11-25 16:35:40 +0900

    useState의 함수형 업데이트와 immer 함께 적용

--- immer-theory/src/App.js
+++ immer-theory/src/App.js
@@ -9,22 +9,20 @@ function App() {
     uselessValue: null,
   });
 
-  const onChange = useCallback(
-    (e) => {
-      const { name, value } = e.target;
+  const onChange = useCallback((e) => {
+    const { name, value } = e.target;
 
-      setForm(
-        // immer를 사용하면 depth가 깊은 상태의 경우에 불변성을 유지하는 작업을 매우 간단하게 처리할 수 있다.
-        // 첫 번째 파라미터는 수정하고 싶은 상태이고, 두 번째 파라미터는 상태를 어떻게 업데이트할지 정의하는 함수
-        // 두 번째 파라미터로 전달되는 함수 내부에서 원하는 값을 변경하면, produce 함수가 불변성 유지를 대신해 주면서 새로운 상태를 생성해 줍니다.
-        //  불변성을 유지하는 코드가 복잡할 때만 사용하자
-        produce(form, (draft) => {
-          draft[name] = value;
-        }),
-      );
-    },
-    [form],
-  );
+    setForm(
+      // immer를 사용하면 depth가 깊은 상태의 경우에 불변성을 유지하는 작업을 매우 간단하게 처리할 수 있다.
+      // 첫 번째 파라미터는 수정하고 싶은 상태이고, 두 번째 파라미터는 상태를 어떻게 업데이트할지 정의하는 함수
+      // 두 번째 파라미터로 전달되는 함수 내부에서 원하는 값을 변경하면, produce 함수가 불변성 유지를 대신해 주면서 새로운 상태를 생성해 줍니다.
+      // immer에서 제공하는 produce 함수를 호출할 때, 첫 번째 파라미터가 함수 형태라면 업데이트 함수를 반환합니다.
+      //  불변성을 유지하는 코드가 복잡할 때만 사용하자
+      produce((draft) => {
+        draft[name] = value;
+      }),
+    );
+  }, []);
 
   const onSubmit = useCallback(
     (e) => {
@@ -37,7 +35,7 @@ function App() {
       };
 
       setData(
-        produce(data, (draft) => {
+        produce((draft) => {
           draft.array.push(info);
         }),
       );
@@ -49,22 +47,19 @@ function App() {
 
       nextId.current += 1;
     },
-    [data, form.name, form.username],
+    [form.name, form.username],
   );
 
-  const onRemove = useCallback(
-    (id) => {
-      setData(
-        produce(data, (draft) => {
-          draft.array.splice(
-            draft.array.findIndex((info) => info.id === id),
-            1,
-          );
-        }),
-      );
-    },
-    [data],
-  );
+  const onRemove = useCallback((id) => {
+    setData(
+      produce((draft) => {
+        draft.array.splice(
+          draft.array.findIndex((info) => info.id === id),
+          1,
+        );
+      }),
+    );
+  }, []);
 
   return (
     <div>
