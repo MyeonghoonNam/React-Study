4095a5ad51ca84ef849d8b1aade17c5327ea0214
MyeongHoonNam <codenmh0822@naver.com>
2021-12-08 23:59:28 +0900

    커스텀 훅을 활용한 데이터 조회 및 결과에 따른 렌더링

--- news-viewer/src/components/NewsList.js
+++ news-viewer/src/components/NewsList.js
@@ -1,7 +1,7 @@
-import { useState, useEffect } from 'react';
 import axios from 'axios';
 import styled from '@emotion/styled';
 import NewsItem from './NewsItem';
+import usePromise from '../hooks/usePromise';
 
 const NewsListBlock = styled.div`
   box-sizing: border-box;
@@ -18,37 +18,31 @@ const NewsListBlock = styled.div`
 `;
 
 const NewsList = ({ category }) => {
-  const [articles, setArticles] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const [loading, response, error] = usePromise(() => {
+    const query = category === 'all' ? '' : `&category=${category}`;
 
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-
-      try {
-        const query = category === 'all' ? '' : `&category=${category}`;
-        const { data } = await axios.get(
-          `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=7cfafbc6441c457e8a96802f5b096f59`,
-        );
-
-        setArticles(data.articles);
-      } catch (e) {
-        console.log(e);
-      }
-
-      setLoading(false);
-    };
-
-    fetchData();
+    return axios.get(
+      `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=7cfafbc6441c457e8a96802f5b096f59`,
+    );
   }, [category]);
 
+  // 대기 중일 때
   if (loading) {
-    return <NewsListBlock>Loading...</NewsListBlock>;
+    return <NewsListBlock>대기 중...</NewsListBlock>;
   }
 
-  if (!articles) {
+  // 아직 response 값이 설정되지 않았을 때
+  if (!response) {
     return null;
   }
+
+  // 에러가 발생했을 때
+  if (error) {
+    return <NewsListBlock>에러 발생!</NewsListBlock>;
+  }
+
+  // response 값이 유효할 때
+  const { articles } = response.data;
 
   return (
     <NewsListBlock>
