47f770bcf76f6440115c29f8d3d0b18eaf3c91d3
MyeongHoonNam <codenmh0822@naver.com>
2021-12-12 20:58:00 +0900

    Todos 컴포넌트 구현

---
+++ redux-theory/src/components/Todos.js
@@ -0,0 +1,26 @@
+import TodoItem from './TodoItems';
+
+const Todos = ({ input, sonChangeInput, onInsert, onToggle, onRemove }) => {
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input />
+        <button type="submit">등록</button>
+      </form>
+
+      <div>
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+      </div>
+    </div>
+  );
+};
+
+export default Todos;
