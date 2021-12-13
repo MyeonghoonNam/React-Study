594eb1cf1cf447941a0c5019189af7bdcffd0b89
MyeongHoonNam <codenmh0822@naver.com>
2021-12-13 23:10:45 +0900

    액션 생성 함수 생성

--- redux-theory/src/modules/counter.js
+++ redux-theory/src/modules/counter.js
@@ -3,3 +3,6 @@
 // 가장 먼저 해야 할 작업은 액션 타입을 정의하는 것입니다. 액션 타입은 대문자로 정의하고, 문자열 내용은 '모듈	이름/액션	이름'과 같은 형태로 작성한다.
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
+
+export const increase = () => ({ type: INCREASE });
+export const decrease = () => ({ type: DECREASE });
