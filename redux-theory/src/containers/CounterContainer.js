e88db00557c89cabc898f0d66c97c0c506c6cfec
MyeongHoonNam <codenmh0822@naver.com>
2021-12-13 23:55:49 +0900

    connect 코드 간소화 방법

--- redux-theory/src/containers/CounterContainer.js
+++ redux-theory/src/containers/CounterContainer.js
@@ -9,20 +9,28 @@ const CounerContainer = ({ number, increase, decrease }) => {
   );
 };
 
-const mapStateToProps = (state) => ({
-  number: state.counter.number,
-});
+// const mapStateToProps = (state) => ({
+//   number: state.counter.number,
+// });
 
-const mapDispatchToProps = (dispatch) => ({
-  increase: () => {
-    dispatch(increase());
-  },
-  decrease: () => {
-    dispatch(decrease());
-  },
-});
+// const mapDispatchToProps = (dispatch) => ({
+//   increase: () => {
+//     dispatch(increase());
+//   },
+//   decrease: () => {
+//     dispatch(decrease());
+//   },
+// });
 
 // mapStateToProps와 mapDispatchToProps에서 반환하는 객체 내부의 값들은 컴포넌트의 props로 전달된다.
 // mapStateToProps는 state를 파라미터로 받아오며, 이 값은 현재 스토어가 지니고 있는 상태를 가리킨다.
 // mapDispatchToProps의 경우 store의 내장 함수 dispatch를 파라미터로 받아온다.
-export default connect(mapStateToProps, mapDispatchToProps)(CounerContainer);
+export default connect(
+  (state) => ({
+    number: state.counter.number,
+  }),
+  {
+    increase,
+    decrease,
+  },
+)(CounerContainer);
