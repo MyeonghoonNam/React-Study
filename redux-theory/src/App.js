c5d9e7b621e3b589e6c5aa5ae3173c812a895211
MyeongHoonNam <codenmh0822@naver.com>
2021-12-13 23:51:50 +0900

    컨테이너 컴포넌트 생성 및 적용

--- redux-theory/src/App.js
+++ redux-theory/src/App.js
@@ -1,10 +1,10 @@
-import Counter from './components/Counter';
+import CounterContainer from './containers/CounterContainer';
 import Todos from './components/Todos';
 
 const App = () => {
   return (
     <div>
-      <Counter number={0} />
+      <CounterContainer />
       <hr />
       <Todos />
     </div>
---
+++ redux-theory/src/containers/CounterContainer.js
@@ -0,0 +1,28 @@
+import Counter from '../components/Counter';
+import { connect } from 'react-redux';
+import { increase, decrease } from '../modules/counter';
+
+//리덕스 스토어와 연동된 컴포넌트를 컨테이너 컴포넌트라고 한다.
+const CounerContainer = ({ number, increase, decrease }) => {
+  return (
+    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+  );
+};
+
+const mapStateToProps = (state) => ({
+  number: state.counter.number,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  increase: () => {
+    dispatch(increase());
+  },
+  decrease: () => {
+    dispatch(decrease());
+  },
+});
+
+// mapStateToProps와 mapDispatchToProps에서 반환하는 객체 내부의 값들은 컴포넌트의 props로 전달된다.
+// mapStateToProps는 state를 파라미터로 받아오며, 이 값은 현재 스토어가 지니고 있는 상태를 가리킨다.
+// mapDispatchToProps의 경우 store의 내장 함수 dispatch를 파라미터로 받아온다.
+export default connect(mapStateToProps, mapDispatchToProps)(CounerContainer);
