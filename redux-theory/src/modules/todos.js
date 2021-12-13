c8a3ee57bf25d7046f26085e9fc9abfd3fb663a7
MyeongHoonNam <codenmh0822@naver.com>
2021-12-13 23:24:35 +0900

    초기 상태 및 리듀서 함수 구현

--- redux-theory/src/modules/todos.js
+++ redux-theory/src/modules/todos.js
@@ -27,3 +27,50 @@ export const remove = (id) => ({
   type: REMOVE,
   id,
 });
+
+const initialState = {
+  input: '',
+  todos: [
+    {
+      id: 1,
+      text: '자바스크립트 학습',
+      done: true,
+    },
+    {
+      id: 2,
+      text: '리액트 학습',
+      done: false,
+    },
+  ],
+};
+
+const todos = (state = initialState, action) => {
+  switch (action.type) {
+    case CHANGE_INPUT:
+      return {
+        ...state,
+        input: action.input,
+      };
+    case INSERT:
+      return {
+        ...state,
+        todos: state.todos.concat(action.todo),
+      };
+    case TOGGLE:
+      return {
+        ...state,
+        todos: state.todos.map((todo) =>
+          todo.id === action.id ? { ...todo, done: !todo.done } : todo,
+        ),
+      };
+    case REMOVE:
+      return {
+        ...state,
+        todos: state.todos.filter((todo) => todo.id !== action.id),
+      };
+    default:
+      return state;
+  }
+};
+
+export default todos;
