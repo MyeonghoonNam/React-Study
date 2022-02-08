41f368e47ee0540c462d2806afd101f1f5ae8799
MyeongHoonNam <codenmh0822@naver.com>
2022-02-08 17:08:57 +0900

    useSelector를 활용한 상태 조회

--- redux-theory/src/containers/CounterContainer.js
+++ redux-theory/src/containers/CounterContainer.js
@@ -1,9 +1,11 @@
 import Counter from '../components/Counter';
-import { connect } from 'react-redux';
+// import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { increase, decrease } from '../modules/counter';
 
 //리덕스 스토어와 연동된 컴포넌트를 컨테이너 컴포넌트라고 한다.
-const CounerContainer = ({ number, increase, decrease }) => {
+const CounterContainer = ({ increase, decrease }) => {
+  const number = useSelector((state) => state.counter.number);
   return (
     <Counter number={number} onIncrease={increase} onDecrease={decrease} />
   );
@@ -25,12 +27,14 @@ const CounerContainer = ({ number, increase, decrease }) => {
 // mapStateToProps와 mapDispatchToProps에서 반환하는 객체 내부의 값들은 컴포넌트의 props로 전달된다.
 // mapStateToProps는 state를 파라미터로 받아오며, 이 값은 현재 스토어가 지니고 있는 상태를 가리킨다.
 // mapDispatchToProps의 경우 store의 내장 함수 dispatch를 파라미터로 받아온다.
-export default connect(
-  (state) => ({
-    number: state.counter.number,
-  }),
-  {
-    increase,
-    decrease,
-  },
-)(CounerContainer);
+// export default connect(
+//   (state) => ({
+//     number: state.counter.number,
+//   }),
+//   {
+//     increase,
+//     decrease,
+//   },
+// )(CounerContainer);
+
+export default CounterContainer;
