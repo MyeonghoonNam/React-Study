96a0b7ac5255f58bb3382f6891bd82d06b6d040e
MyeongHoonNam <codenmh0822@naver.com>
2022-02-08 17:02:16 +0900

    immer를 활용한 복잡한 객체의 불변성 유지 반영

--- redux-theory/src/modules/todos.js
+++ redux-theory/src/modules/todos.js
@@ -1,4 +1,5 @@
-import { createAction, handleAction } from 'redux-actions';
+import { createAction, handleActions } from 'redux-actions';
+import produce from 'immer';
 
 const CHANGE_INPUT = 'todos/CHANGE_INPUT';
 const INSERT = 'todos/INSERT';
@@ -34,23 +35,26 @@ const initialState = {
   ],
 };
 
-const todos = handleAction(
+const todos = handleActions(
   {
-    [CHANGE_INPUT]: (state, { payload: input }) => ({ ...state, input }),
-    [INSERT]: (state, { payload: todo }) => ({
-      ...state,
-      todos: state.todos.concat(todo),
-    }),
-    [TOGGLE]: (state, { payload: id }) => ({
-      ...state,
-      todos: state.todos.map((todo) =>
-        todo.id === id ? { ...todo, doe: !todo.done } : todo,
-      ),
-    }),
-    [REMOVE]: (state, { payload: id }) => ({
-      ...state,
-      todos: state.todos.fliter((todo) => todo.id !== id),
-    }),
+    [CHANGE_INPUT]: (state, { payload: input }) =>
+      produce(state, (draft) => {
+        draft.input = input;
+      }),
+    [INSERT]: (state, { payload: todo }) =>
+      produce(state, (draft) => {
+        draft.todos.push(todo);
+      }),
+    [TOGGLE]: (state, { payload: id }) =>
+      produce(state, (draft) => {
+        const todo = draft.todos.find((todo) => todo.id === id);
+        todo.done = !todo.done;
+      }),
+    [REMOVE]: (state, { payload: id }) =>
+      produce(state, (draft) => {
+        const removeTodoIndex = draft.todos.findIndex((todo) => todo.id === id);
+        draft.todos.splice(removeTodoIndex, 1);
+      }),
   },
   initialState,
 );
