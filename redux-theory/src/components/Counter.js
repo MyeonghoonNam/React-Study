a19fa5566803df5fbab530411a691c4c5cb31656
MyeongHoonNam <codenmh0822@naver.com>
2022-02-08 15:08:26 +0900

    불필요 코드 제거

--- redux-theory/src/components/Counter.js
+++ redux-theory/src/components/Counter.js
@@ -1,13 +1,11 @@
 const Counter = ({ number, onIncrease, onDecrease }) => {
   return (
     <div>
-      {' '}
-      <h1>{number}</h1>{' '}
+      <h1>{number}</h1>
       <div>
-        {' '}
-        <button onClick={onIncrease}>+1</button>{' '}
-        <button onClick={onDecrease}>-1</button>{' '}
-      </div>{' '}
+        <button onClick={onIncrease}>+1</button>
+        <button onClick={onDecrease}>-1</button>
+      </div>
     </div>
   );
 };
