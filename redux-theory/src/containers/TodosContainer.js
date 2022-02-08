e4f97daabbacdaca487a8321c9b570cc09c55b35
MyeongHoonNam <codenmh0822@naver.com>
2022-02-08 17:42:56 +0900

    커스텀 훅을 활용한 액션 생성함수 수정

--- redux-theory/src/containers/TodosContainer.js
+++ redux-theory/src/containers/TodosContainer.js
@@ -1,39 +1,19 @@
 // import { connect } from 'react-redux';
-import React, { useCallback } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { changeInput, insert, toggle, remove } from '../modules/todos';
 import Todos from '../components/Todos';
+import useActions from '../hooks/useActions';
 
 const TodosContainer = () => {
   const { input, todos } = useSelector(({ todos }) => ({
     input: todos.input,
     todos: todos.todos,
   }));
-  const dispatch = useDispatch();
-  const onChangeInput = useCallback(
-    (input) => {
-      dispatch(changeInput(input));
-    },
-    [dispatch],
+  const [onChangeInput, onInsert, onToggle, onRemove] = useActions(
+    [changeInput, insert, toggle, remove],
+    [],
   );
-  const onInsert = useCallback(
-    (text) => {
-      dispatch(insert(text));
-    },
-    [dispatch],
-  );
-  const onToggle = useCallback(
-    (id) => {
-      dispatch(toggle(id));
-    },
-    [dispatch],
-  );
-  const onRemove = useCallback(
-    (id) => {
-      dispatch(remove(id));
-    },
-    [dispatch],
-  );
+
   return (
     <Todos
       input={input}
