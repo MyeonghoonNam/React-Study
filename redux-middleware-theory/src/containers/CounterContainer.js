4a3cfc99ebfb350fba274e94fe094072e9d303f6
MyeongHoonNam <codenmh0822@naver.com>
2022-02-10 23:11:07 +0900

    hook을 활용한 상태관리 코드 수정

--- redux-middleware-theory/src/containers/CounterContainer.js
+++ redux-middleware-theory/src/containers/CounterContainer.js
@@ -1,14 +1,20 @@
-import { connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { increase, decrease } from '../modules/counter';
 import Counter from '../components/Counter';
+import React, { useCallback } from 'react';
 
-const CounterContainer = ({ number, increase, decrease }) => {
+const CounterContainer = () => {
+  const number = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  const onIncrease = useCallback(() => {
+    dispatch(increase());
+  }, [dispatch]);
+  const onDecrease = useCallback(() => {
+    dispatch(decrease());
+  }, [dispatch]);
   return (
-    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+    <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />
   );
 };
 
-export default connect((state) => ({ number: state.counter }), {
-  increase,
-  decrease,
-})(CounterContainer);
+export default React.memo(CounterContainer);
