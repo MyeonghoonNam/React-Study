98f12cfae4d38c58fb9705598efc816f9b345a4b
MyeongHoonNam <codenmh0822@naver.com>
2022-02-11 19:01:25 +0900

    thunk 생성함수를 util 함수로 모듈화하여 리펙토링

--- redux-middleware-theory/src/modules/webApiAsync.js
+++ redux-middleware-theory/src/modules/webApiAsync.js
@@ -1,5 +1,6 @@
 import { handleActions } from 'redux-actions';
 import * as api from '../utils/api';
+import createRequestThunk from '../utils/createRequestThunk';
 
 // 액션 타입
 const GET_POST = 'sample/GET_POST';
@@ -13,49 +14,8 @@ const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
 // thunk 함수
 // thunk 함수 내부에서는 시작, 성공, 실패의 경우 각각 다른 액션을 디스패치 한다.
 
-export const getPost = (id) => async (dispatch) => {
-  dispatch({ type: GET_POST }); // 요청 시작
-
-  try {
-    const response = await api.getPost(id);
-
-    dispatch({
-      type: GET_POST_SUCCESS,
-      payload: response.data,
-    }); // 요청 성공
-  } catch (e) {
-    console.log(e);
-
-    dispatch({
-      type: GET_POST_FAILURE,
-      payload: e,
-      error: true,
-    }); // 에러 발생
-
-    throw e; // 프레젠테이셔널 컴포넌트단으로 에러 전달
-  }
-};
-
-export const getUsers = () => async (dispatch) => {
-  dispatch({ type: GET_USERS });
-
-  try {
-    const response = await api.getUsers();
-
-    dispatch({
-      type: GET_USERS_SUCCESS,
-      payload: response.data,
-    });
-  } catch (e) {
-    dispatch({
-      type: GET_USERS_FAILURE,
-      payload: e,
-      error: true,
-    });
-
-    throw e;
-  }
-};
+export const getPost = createRequestThunk(GET_POST, api.getPost);
+export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
 
 const initialState = {
   loading: {
---
+++ redux-middleware-theory/src/utils/createRequestThunk.js
@@ -0,0 +1,27 @@
+const createRequestThunk = (type, request) => {
+  const SUCCESS = `${type}_SUCCESS`;
+  const FAILURE = `${type}_FAILURE`;
+
+  return (params) => async (dispatch) => {
+    dispatch({ type });
+
+    try {
+      const response = await request(params);
+
+      dispatch({
+        type: SUCCESS,
+        payload: response.data,
+      });
+    } catch (e) {
+      dispatch({
+        type: FAILURE,
+        payload: e,
+        error: true,
+      });
+
+      throw e;
+    }
+  };
+};
+
+export default createRequestThunk;
