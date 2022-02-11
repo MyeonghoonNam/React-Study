8ec6850c031590a5a07d2602c8be1260bea06d20
MyeongHoonNam <codenmh0822@naver.com>
2022-02-11 19:26:01 +0900

    loading 관련 로직 모듈화

--- redux-middleware-theory/src/modules/index.js
+++ redux-middleware-theory/src/modules/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
 import counter from './counter';
 import webApiAsync from './webApiAsync';
+import loading from './loading';
 
-const rootReducer = combineReducers({ counter, webApiAsync });
+const rootReducer = combineReducers({ counter, webApiAsync, loading });
 
 export default rootReducer;
