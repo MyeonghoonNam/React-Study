cb8cfab6a380d610cf12552145aee3de23804c41
MyeongHoonNam <codenmh0822@naver.com>
2022-02-11 18:35:29 +0900

    return 값 지정

--- redux-middleware-theory/src/utils/api.js
+++ redux-middleware-theory/src/utils/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export const getPost = (id) => {
-  axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  return axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`);
 };
 
 export const getUsers = () => {
-  axios.get(`https://jsonplaceholder.typicode.com/users`);
+  return axios.get(`https://jsonplaceholder.typicode.com/users`);
 };
