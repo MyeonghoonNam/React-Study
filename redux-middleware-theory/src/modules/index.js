d81ade90554741979535f727085a7cdbbcb6f868
MyeongHoonNam <codenmh0822@naver.com>
2022-02-13 22:31:06 +0900

    react-saga 루트 리듀서 구현

--- redux-middleware-theory/src/modules/index.js
+++ redux-middleware-theory/src/modules/index.js
@@ -1,8 +1,28 @@
+// import { combineReducers } from 'redux';
+// import counter from './counter';
+// import webApiAsync from './webApiAsync';
+// import loading from './loading';
+
+// const rootReducer = combineReducers({ counter, webApiAsync, loading });
+
+// export default rootReducer;
+
+// ----------------------------------- react-saga
 import { combineReducers } from 'redux';
-import counter from './counter';
-import webApiAsync from './webApiAsync';
+import { all } from 'redux-saga/effects';
+import counter, { counterSaga } from './counter';
+import sample from './sample';
 import loading from './loading';
 
-const rootReducer = combineReducers({ counter, webApiAsync, loading });
+const rootReducer = combineReducers({
+  counter,
+  sample,
+  loading,
+});
+
+export function* rootSaga() {
+  // all 함수는 여러 사가를 합쳐 주는 역할
+  yield all([counterSaga()]);
+}
 
 export default rootReducer;
