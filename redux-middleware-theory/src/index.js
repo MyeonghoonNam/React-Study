da0b08af7dbbc8363be56440eb03a98fe2fab73d
MyeongHoonNam <codenmh0822@naver.com>
2022-02-15 17:29:57 +0900

    redux 개발자 도구 활성화

--- redux-middleware-theory/src/index.js
+++ redux-middleware-theory/src/index.js
@@ -9,12 +9,13 @@ import rootReducer, { rootSaga } from './modules';
 import { createLogger } from 'redux-logger';
 import ReduxThunk from 'redux-thunk';
 import createSagaMiddleware from 'redux-saga';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const logger = createLogger();
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(
   rootReducer,
-  applyMiddleware(logger, ReduxThunk, sagaMiddleware),
+  composeWithDevTools(applyMiddleware(logger, ReduxThunk, sagaMiddleware)),
 );
 sagaMiddleware.run(rootSaga);
 
