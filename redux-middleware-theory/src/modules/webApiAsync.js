cca245301e6bf9b08e8e4c2cd17b5569a0c527d1
MyeongHoonNam <codenmh0822@naver.com>
2022-02-15 18:16:18 +0900

    util saga 모듈화를 통한 코드 축소 리팩토링

--- redux-middleware-theory/src/modules/webApiAsync.js
+++ redux-middleware-theory/src/modules/webApiAsync.js
@@ -38,65 +38,22 @@
 
 // redux-saga
 import { createAction, handleActions } from 'redux-actions';
-import { call, put, takeLatest } from 'redux-saga/effects';
+import { takeLatest } from 'redux-saga/effects';
 import * as api from '../utils/api';
-import { startLoading, finishLoading } from './loading';
+import createRequestSaga from '../utils/createRequestSaga';
 
 // Action Type
 const GET_POST = 'sample/GET_POST';
 const GET_POST_SUCCESS = 'sample/GET_POST_SUCCESS';
-const GET_POST_FAILURE = 'sample/GET_POST_FAILURE';
 
 const GET_USERS = 'sample/GET_USERS';
 const GET_USERS_SUCCESS = 'sample/GET_USERS_SUCCESS';
-const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
 
 export const getPost = createAction(GET_POST, (id) => id);
 export const getUsers = createAction(GET_USERS);
 
-function* getPostSaga(action) {
-  yield put(startLoading(GET_POST));
-
-  try {
-    // call함수 : Promise를 반환하는 함수를 호출하고 대기
-    // 첫 번째 파라미터는 비동기 함수, 나머지는 첫 번째 함수에 넣을 인수
-    const post = yield call(api.getPost, action.payload); // const post = api.getPost(action.payload)를 의미한다.
-
-    yield put({
-      type: GET_POST_SUCCESS,
-      payload: post.data,
-    });
-  } catch (e) {
-    yield put({
-      type: GET_POST_FAILURE,
-      payload: e,
-      error: true,
-    });
-  }
-
-  yield put(finishLoading(GET_POST));
-}
-
-function* getUsersSaga() {
-  yield put(startLoading(GET_USERS));
-
-  try {
-    const users = yield call(api.getUsers);
-
-    yield put({
-      type: GET_USERS_SUCCESS,
-      payload: users.data,
-    });
-  } catch (e) {
-    yield put({
-      type: GET_USERS_FAILURE,
-      payload: e,
-      error: true,
-    });
-  }
-
-  yield put(finishLoading(GET_USERS));
-}
+const getPostSaga = createRequestSaga(GET_POST, api.getPost);
+const getUsersSaga = createRequestSaga(GET_USERS, api.getUsers);
 
 export function* webApiAsyncSaga() {
   yield takeLatest(GET_POST, getPostSaga);
