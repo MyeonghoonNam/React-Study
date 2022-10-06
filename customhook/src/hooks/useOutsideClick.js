9902bd2e3a0754b321f4abdce2ef4a3bcd56a164
MyeongHoonNam <codenmh0822@naver.com>
2022-10-06 19:38:42 +0900

    feat : useOutsideClick custom hook 구현

---
+++ customhook/src/hooks/useOutsideClick.js
@@ -0,0 +1,21 @@
+import { useEffect } from 'react';
+
+const useOutsidClick = (ref, handler) => {
+  useEffect(() => {
+    const listener = (e) => {
+      if (!ref.current || ref.current.contains(e.target)) {
+        return;
+      }
+
+      handler(e);
+    };
+
+    document.addEventListener('mousedown', listener);
+
+    return () => {
+      document.removeEventListener('mousedown', listener);
+    };
+  }, [ref, handler]);
+};
+
+export default useOutsidClick;
