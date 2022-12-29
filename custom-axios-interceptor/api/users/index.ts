d1f5f0904b74a91e1eaa2aa477fa0c4f5b77e7ac
MyeongHoonNam <codenmh0822@naver.com>
2022-12-29 18:08:30 +0900

    feat : 사용자 정보 조회 api 구현

--- custom-axios-interceptor/api/users/index.ts
+++ custom-axios-interceptor/api/users/index.ts
@@ -4,3 +4,6 @@ import type { User } from './types';
 const RESOURCE = 'users';
 
 export const getUsers = () => config.get<User[]>(`${RESOURCE}`);
+
+export const getUser = (userId: number) =>
+  config.get<User>(`${RESOURCE}/${userId}`);
