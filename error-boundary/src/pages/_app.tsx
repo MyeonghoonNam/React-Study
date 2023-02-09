0cd917237b5cb4747086e2ea62786fc3c323111d
MyeongHoonNam <codenmh0822@naver.com>
2023-02-09 16:36:00 +0900

    chore : react-query install

--- error-boundary/src/pages/_app.tsx
+++ error-boundary/src/pages/_app.tsx
@@ -1,5 +1,14 @@
 import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
