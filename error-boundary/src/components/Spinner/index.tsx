0493fa37cc4150e0e6dca15447129b77921121f4
MyeongHoonNam <codenmh0822@naver.com>
2023-02-18 15:31:00 +0900

    feat : Spinner 컴포넌트 구현

---
+++ error-boundary/src/components/Spinner/index.tsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+import { FaSpinner } from 'react-icons/fa';
+
+const Spinner = () => {
+  console.log('Spinner');
+  return (
+    <Container>
+      <FaSpinner />
+    </Container>
+  );
+};
+
+const Container = styled.i`
+  font-size: 20px;
+  animation: spin 2s linear infinite;
+  display: inline-block;
+  align-self: center;
+
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
+export default Spinner;
