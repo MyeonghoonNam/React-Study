4d1679c6b198e39c373c01ef64ad7fd7f0269e27
MyeongHoonNam <codenmh0822@naver.com>
2023-02-23 16:53:43 +0900

    refactor : 리소스 주석 추가

--- error-boundary/src/api/user/index.ts
+++ error-boundary/src/api/user/index.ts
@@ -1,6 +1,7 @@
 import config from '../config';
 import type { User } from './types';
 
-const RESOURCE = '/users';
+const RESOURCE = '/users'; // 정상적인 리소스
+// const RESOURCE = '/users'; // 에러를 유발하는 잘못된 리소스
 
 export const getUserList = () => config.get<User[]>(`${RESOURCE}`);
